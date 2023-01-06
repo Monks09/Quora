81a540793c84d63f0d544cf75d1a83e4a52aa8c6
Monks09 <mayanksharma5104@gmail.com>
2023-01-06 12:02:28 +0530

    changes

--- src/Pages/Answer/Answer.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Answer.module.css';
-
-export default function Answer() {
-    return <div className={styles.Answer}>
-        
-    </div>
-}
--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
