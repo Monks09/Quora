e7b1f8d09315e049a06339500037d4e46af060c1
Monks09 <mayanksharma5104@gmail.com>
2023-01-09 11:32:53 +0530

    added login page

--- src/Component/Allroutes.jsx
+++ src/Component/Allroutes.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
-import Home from "./Home/Home"
 import Following from "../Pages/Folllowing/Following";
 import Answer from "../Pages/Answer/Answer";
 import { Route,Routes } from "react-router-dom";
 import Private from "./Home/Private";
+import Auth from "./Auth/Auth";
 function Allroutes() {
   return (
     <div>
-      <Navbar/>
       {/*  */}
       {/* <Answer/> */}
       {/* <Home/> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Auth />} />
         <Route path="/answer" element={<Answer/>} />
         <Route path="/following" element={<Following/>} />
         <Route path="/post/:url" element={<Private/>} />
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -8,30 +8,35 @@ import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
 import ArticleIcon from "@mui/icons-material/Article";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import loginContext from "../Context/Context";
+
 function Navbar() {
+  const { state } = useContext(loginContext);
   const [input, setInput] = useState("");
   const [inputUrl, setInputUrl] = useState("");
-  return (
+
+  return state.isLoggedIn ? (
     <div className="qHeader">
-      
-        <Link className ='qHeader__logo' to = {'/'}>
+
+      <Link className='qHeader__logo' to={'/'}>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Quora_logo_2015.svg/250px-Quora_logo_2015.svg.png"
           alt=""
         />
-         </Link>
-       
+      </Link>
+
       <div className="qHeader__icons">
-            <Link className="active qHeader__icon" to={'/'}><Home /></Link>
-        
-            <Link className="qHeader__icon"  to={'/following'}> <ArticleIcon /></Link>
+        <Link className="active qHeader__icon" to={'/'}><Home /></Link>
 
-            <Link className="qHeader__icon"  to={'/answer'}> <AssignmentTurnedInIcon /></Link>
+        <Link className="qHeader__icon" to={'/following'}> <ArticleIcon /></Link>
 
-            <Link className="qHeader__icon" > <PeopleAltIcon /></Link>
-            
-            <Link className="qHeader__icon" > <NotificationsNoneIcon /></Link>
-        
+        <Link className="qHeader__icon" to={'/answer'}> <AssignmentTurnedInIcon /></Link>
+
+        <Link className="qHeader__icon" > <PeopleAltIcon /></Link>
+
+        <Link className="qHeader__icon" > <NotificationsNoneIcon /></Link>
+
       </div>
       <div className="qHeader__input">
         <SearchIcon />
@@ -49,7 +54,7 @@ function Navbar() {
         <button>Add Question</button>
       </div>
     </div>
-  );
+  ) : ( "" );
 }
 
 export default Navbar;
