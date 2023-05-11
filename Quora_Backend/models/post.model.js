a0457cfcc9ba39339df50871b336c12b2caf230f
Monks09 <mayanksharma5104@gmail.com>
2023-05-11 21:18:47 +0530

    removed answer model

--- Quora_Backend/models/post.model.js
+++ Quora_Backend/models/post.model.js
@@ -4,11 +4,15 @@ const PostSchema = new mongoose.Schema(
     {
         body: { type: String, required: true },
         image: { type: String, required: false },
-        topic: { type: String, required: true },
         author: {
             userId: { type: String, required: true },
             name: { type: String, required: true },
             avatar: { type: String, required: false },
+        },
+        question: {
+            quesId: { type: String, required: true },
+            title: { type: String, required: true },
+            topic: { type: String, required: true },
         },
         views: { type: Number, required: true },
         upvotes: { type: Number, required: true },
