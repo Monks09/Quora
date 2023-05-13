47cfc84673471d0da6848ea86f8d8d7ddb2fbaca
Monks09 <mayanksharma5104@gmail.com>
2023-05-13 17:19:19 +0530

    moved backend to another repo

--- Quora_Backend/config/configs.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const configs = {
-    CONNECTION_URL: process.env.CONNECTION_URL,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-}
-
-module.exports = configs;
--- Quora_Backend/controllers/following.controller.js
+++
@@ -1,9 +0,0 @@
-const Space = require("../models/space.model")
-
-const getSpaces = async () => {
-    let spaces = await Space.find();
-    return spaces;
-}
-
-
-module.exports = { getSpaces }
